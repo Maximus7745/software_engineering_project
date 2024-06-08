e54bc0fd652fb6d49e522f01a5bce48815fc6d91
zenec4 <147539787+zenec4@users.noreply.github.com>
2024-06-09 03:47:57 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,3 @@
-token_name = "filename.json"
+token_name = 'client_secret_251619734123-9l0i54dq8vdjkrb7vijq8mffrgseonmp.apps.googleusercontent.com.json'
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+CHECK_INTERVAL = 120000
