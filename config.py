5677f5a7db1765222689cbe062e1e471dd509517
Maximus7745 <104903612+Maximus7745@users.noreply.github.com>
2024-06-09 04:29:58 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-token_name = 'client_secret_251619734123-9l0i54dq8vdjkrb7vijq8mffrgseonmp.apps.googleusercontent.com.json'
+token_name = 'token_name.json'
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 CHECK_INTERVAL = 120000
